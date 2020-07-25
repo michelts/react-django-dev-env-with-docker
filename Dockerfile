cd84d46a7c5fc5348df07e4cb58a6bb62d6b4ec7
Michel Sabchuk <michel@turbosys.com.br>
2020-07-25 14:41:48 -0300

    Configure django-webpack-loader

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:lts-alpine as front
 
 FROM python:alpine
-RUN pip3 install Django
+RUN pip3 install Django django-webpack-loader==0.7.0
 WORKDIR /srv/backend/
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': './',
+        'STATS_FILE': os.path.join(
+            os.path.dirname(BASE_DIR),
+            'bundles/frontend-stats.json'
+        ),
+    },
+}
